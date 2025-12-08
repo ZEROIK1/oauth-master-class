739d422ccd128cc15b5741d608f17439687aebb9
ZEROIK1 <lordnais@ya.ru>
2025-12-08 07:28:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ window.onload = () => {
   document.getElementById("button").onclick = () => {
     window.YaAuthSuggest.init(
       {
-        client_id: "c46f0c53093440c39f12eff95a9f2f93",
+        client_id: "734cca9a236d4485bc6de5df7c0b85cb",
         response_type: "token",
         redirect_uri: "https://examplesite.com/suggest/token",
       },
